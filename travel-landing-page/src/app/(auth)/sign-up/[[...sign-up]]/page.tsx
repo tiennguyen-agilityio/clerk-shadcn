90bb1be1edf5b537e7f92fc97c4a8f26c04a0018
Tien Nguyen <tien.nguyen@asnet.com.vn>
2025-07-02 17:33:12 +0700

    Implement SignUp page

---
+++ travel-landing-page/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs";
+
+import Loading from "@components/Loading";
+
+const SignUpPage = () => {
+  return <SignUp fallback={<Loading />} />;
+};
+
+export default SignUpPage;
