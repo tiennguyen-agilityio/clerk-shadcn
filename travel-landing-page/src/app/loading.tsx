cf9150fae276b87084a50ca374e4e1612183d062
Tien Nguyen <tien.nguyen@asnet.com.vn>
2025-07-03 10:19:47 +0700

    Implement Home page

---
+++ travel-landing-page/src/app/loading.tsx
@@ -0,0 +1,5 @@
+import { Loading } from "@components";
+
+export default function LoadingPage() {
+  return <Loading />;
+}
--- travel-landing-page/src/app/page.tsx
+++ travel-landing-page/src/app/page.tsx
@@ -1,30 +1,29 @@
-import { SignInButton, SignOutButton, SignUpButton } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
+"use client";
 
-export default async function Home() {
-  const { userId } = await auth();
+import { RedirectToSignIn, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
-  console.log("userId", userId);
-
-  if (!userId) {
-    return (
-      <div>
-        <div>Sign in to view this page</div>
-        <div>
-          <SignInButton />
-        </div>
-        <div>
-          <SignUpButton />
-        </div>
-      </div>
-    );
-  }
-
+export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>Heading</h1>
-        <SignOutButton />
+        <SignedIn>
+          <h1>Home page</h1>
+
+          <UserButton
+            showName
+            appearance={{
+              elements: {
+                userButton: "rounded-md border border-gray-300 shadow p-2",
+                userButtonTrigger: "bg-red-200",
+                userButtonOuterIdentifier: "text-white pr-2",
+              },
+            }}
+          />
+        </SignedIn>
+
+        <SignedOut>
+          <RedirectToSignIn />
+        </SignedOut>
       </main>
     </div>
   );
