ef0633b86dfc0bcb3d864d02a512d0b5aad588eb
Tien Nguyen <tien.nguyen@asnet.com.vn>
2025-07-08 10:06:43 +0700

    Update Header component

--- travel-landing-page/src/components/Header/index.tsx
+++ travel-landing-page/src/components/Header/index.tsx
@@ -16,56 +16,49 @@ const Header = () => {
   const pathname = usePathname() || "";
   const isActiveSignIn = pathname.startsWith("/sign-in");
   const isActiveSignUp = pathname.startsWith("/sign-up");
+  const isHasBorder = ["/sign-in", "/sign-up"].includes(pathname);
 
   return (
-    <div className="flex items-center content-center mx-auto h-25 max-w-7xl">
-      <Logo className="w-auto" />
-      <div className="flex ml-auto mr-0 items-center">
-        <NavBar list={NAV_BAR} href={pathname} />
-        <Button className="ml-20 mr-4">
-          <BookmarkIcon />
-          Reservations
-        </Button>
-        <SearchIcon />
-        <div className="flex justify-end min-w-20 ml-5">
-          <SignedOut>
-            <div className="flex items-center font-acme text-[13px] text-white">
-              <SignInButton>
-                <span
-                  className={clsx(
-                    "cursor-pointer text-white-500",
-                    isActiveSignIn && "text-orange-500"
-                  )}
-                >
-                  Sign In
-                </span>
-              </SignInButton>
-              /
-              <SignUpButton>
-                <span
-                  className={clsx(
-                    "cursor-pointer text-white-500",
-                    isActiveSignUp && "text-orange-500"
-                  )}
-                >
-                  Sign Up
-                </span>
-              </SignUpButton>
-            </div>
-          </SignedOut>
-          <SignedIn>
-            <UserButton
-              userProfileMode="navigation"
-              userProfileUrl="/user/profile"
-              appearance={{
-                elements: {
-                  userButton: "rounded-md border border-gray-300 shadow p-2",
-                  userButtonTrigger: "bg-red-200",
-                  userButtonOuterIdentifier: "text-white pr-2",
-                },
-              }}
-            />
-          </SignedIn>
+    <div className={clsx("w-full", isHasBorder && "border-b-1 border-[#d8d8d8]")}>
+      <div className="flex items-center content-center mx-auto h-25 max-w-7xl">
+        <Logo className="w-auto" />
+        <div className="flex ml-auto mr-0 items-center">
+          <NavBar list={NAV_BAR} href={pathname} />
+          <Button className="ml-20 mr-4">
+            <BookmarkIcon />
+            Reservations
+          </Button>
+          <SearchIcon />
+          <div className="flex justify-end min-w-20 ml-5">
+            <SignedOut>
+              <div className="flex items-center font-acme text-[13px]">
+                <SignInButton>
+                  <span className={clsx("cursor-pointer", isActiveSignIn && "text-orange-500")}>
+                    Sign In
+                  </span>
+                </SignInButton>
+                /
+                <SignUpButton>
+                  <span className={clsx("cursor-pointer", isActiveSignUp && "text-orange-500")}>
+                    Sign Up
+                  </span>
+                </SignUpButton>
+              </div>
+            </SignedOut>
+            <SignedIn>
+              <UserButton
+                userProfileMode="navigation"
+                userProfileUrl="/user/profile"
+                appearance={{
+                  elements: {
+                    userButton: "rounded-md border border-gray-300 shadow p-2",
+                    userButtonTrigger: "bg-red-200",
+                    userButtonOuterIdentifier: "text-white pr-2",
+                  },
+                }}
+              />
+            </SignedIn>
+          </div>
         </div>
       </div>
     </div>
