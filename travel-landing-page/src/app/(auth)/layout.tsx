fdd62113dcb53a74acc914c4b4a69bf15c873dd5
Tien Nguyen <tien.nguyen@asnet.com.vn>
2025-07-09 17:27:42 +0700

    Implement UI for SignIn page

--- travel-landing-page/src/app/(auth)/layout.tsx
+++ travel-landing-page/src/app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ const AuthLayout = ({
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  return <div className="flex flex-col items-center justify-center h-fit">{children}</div>;
+  return <div className="flex flex-col items-center justify-center h-fit py-15">{children}</div>;
 };
 
 export default AuthLayout;
--- travel-landing-page/src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ travel-landing-page/src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,45 +1,112 @@
 "use client";
 
-import { useEffect } from "react";
-import { SignIn } from "@clerk/nextjs";
+import { useState } from "react";
+import { Button, FacebookIcon, GoogleIcon, Loading } from "@/components";
+import * as SignIn from "@clerk/elements/sign-in";
+import * as Clerk from "@clerk/elements/common";
 
 const SignInPage = () => {
-  useEffect(() => {
-    const updateFeedback = () => {
-      document.querySelectorAll("input").forEach((input) => {
-        if (input.validity.valid) {
-          input.setAttribute("data-feedback", "info");
-          input.setAttribute("aria-invalid", "false");
-        } else {
-          input.setAttribute("data-feedback", "error");
-          input.setAttribute("aria-invalid", "true");
-        }
-      });
-    };
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
-    document.addEventListener("input", updateFeedback);
-    document.addEventListener("blur", updateFeedback, true);
-
-    return () => {
-      document.removeEventListener("input", updateFeedback);
-      document.removeEventListener("blur", updateFeedback, true);
-    };
-  }, []);
+  const handleToggleVisible = () => setIsPasswordVisible((prev) => !prev);
 
   return (
-    <SignIn
-      appearance={{
-        elements: {
-          // formButtonPrimary: {
-          // 	height: "55px",
-          // },
-          formButtonPrimary: "bg-slate-500 hover:bg-slate-400 text-sm",
-        },
-      }}
-      fallback={<div className="bg-red-50"> Loading</div>}
-      // oauthFlow="redirect"
-      // path="/sign-in"
-    />
+    <div className="font-acme text-sm">
+      <SignIn.Root fallback={<Loading />}>
+        <SignIn.Step name="start">
+          <div className="flex-col justify-center align-middle items-center content-center w-[545px]">
+            <h1 className="">Sign In</h1>
+            <div className="flex justify-between mt-12.5 h-12.5 gap-2.5 text-white">
+              <Clerk.Connection
+                name="facebook"
+                className="flex justify-center items-center gap-5 w-1/2  bg-[#3b5998] hover:bg-blue-900 transition-colors duration-300 rounded-sm"
+              >
+                <FacebookIcon />
+                <div className="border-l-[1px] h-5 border-foreground" />
+                <Clerk.Loading scope="provider:facebook">
+                  {(isLoading) => (isLoading ? "Loading..." : "Sign in with Facebook")}
+                </Clerk.Loading>
+              </Clerk.Connection>
+              <Clerk.Connection
+                name="google"
+                className="flex justify-center items-center gap-2.5 w-1/2 bg-[#db3236] hover:bg-red-700 transition-colors duration-300 rounded-sm"
+              >
+                <GoogleIcon />
+                <div className="border-l-[1px] h-5 border-foreground" />
+                <Clerk.Loading scope="provider:google">
+                  {(isLoading) => (isLoading ? "Loading..." : "Sign in with Google")}
+                </Clerk.Loading>
+              </Clerk.Connection>
+            </div>
+
+            <div className="flex flex-col gap-7.5">
+              <div className="flex flex-col justify-center items-center h-10 relative pt-5">
+                <div className="h-[1px] bg-[#d8d8d8] absolute z-1 w-full" />
+                <span className="flex items-center h-2.5 px-3 bg-white z-2">OR</span>
+              </div>
+
+              <Clerk.Field name="identifier">
+                <Clerk.Label className="mb-1.25">Email</Clerk.Label>
+                <Clerk.Input
+                  placeholder="Enter Email"
+                  className="w-full rounded border border-gray-300 px-3 py-2 text-sm placeholder-gray-400 focus:border-orange-500 focus:outline-none focus:ring-1 focus:ring-orange-500"
+                />
+                <Clerk.FieldError className="text-red-500" />
+              </Clerk.Field>
+
+              <Clerk.Field name="password">
+                <Clerk.Label>Password</Clerk.Label>
+                <div className="relative">
+                  <Clerk.Input
+                    type={isPasswordVisible ? "text" : "password"}
+                    validatePassword
+                    placeholder="Enter password"
+                    className="w-full rounded border border-gray-300 px-3 py-2 text-sm placeholder-gray-400 focus:border-orange-500 focus:outline-none focus:ring-1 focus:ring-orange-500"
+                  />
+                  <button
+                    type="button"
+                    className="absolute right-3 h-full text-[#4a90e2] hover:text-blue-900"
+                    onClick={handleToggleVisible}
+                  >
+                    {isPasswordVisible ? "Hide Password" : "Show Password"}
+                  </button>
+                </div>
+                <Clerk.FieldError className="text-red-500" />
+              </Clerk.Field>
+
+              <div className="flex justify-between">
+                <div className="flex gap-2.5">
+                  <input id="remember" type="checkbox" />
+                  <label htmlFor="remember">Remember me</label>
+                </div>
+
+                <SignIn.Action
+                  navigate="forgot-password"
+                  className="text-[#4a90e2] hover:text-blue-900"
+                >
+                  Forgot password?
+                </SignIn.Action>
+              </div>
+
+              <SignIn.Action submit asChild>
+                <Button className="rounded-sm w-full text-center items-center justify-center text-sm">
+                  <Clerk.Loading>
+                    {(isLoading) => (isLoading ? "Submitting..." : " Sign In")}
+                  </Clerk.Loading>
+                </Button>
+              </SignIn.Action>
+
+              <div className="flex justify-center text-center gap">
+                Don&#8217;t have an account?&nbsp;
+                <Clerk.Link navigate="sign-up" className="text-[#4a90e2] hover:text-blue-900">
+                  Sign up
+                </Clerk.Link>
+              </div>
+            </div>
+          </div>
+        </SignIn.Step>
+      </SignIn.Root>
+    </div>
   );
 };
 
