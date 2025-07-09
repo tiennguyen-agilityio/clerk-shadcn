5bb7d1df6df861821c121457ed3f752c54844b70
Tien Nguyen <tien.nguyen@asnet.com.vn>
2025-07-09 16:50:21 +0700

    Add FacebookIcon, GoogleIcon component

--- travel-landing-page/src/components/Button/index.tsx
+++ travel-landing-page/src/components/Button/index.tsx
@@ -11,7 +11,7 @@ const Button = ({ children, className, onClick }: ButtonProps) => {
   return (
     <button
       className={clsx(
-        "flex items-center gap-2 px-6 py-3 rounded-full bg-orange-600 text-current font-acme text-[13px] hover:bg-orange-700 transition-colors duration-300",
+        "flex items-center gap-2 px-6 py-3 rounded-full bg-orange-600 text-white font-acme text-[13px] hover:bg-orange-700 transition-colors duration-300",
         className
       )}
       onClick={onClick}
---
+++ travel-landing-page/src/components/Icons/FacebookIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { SVGProps } from "@/types";
+
+const FacebookIcon = ({ width = 8, height = 16, color = "white", onClick }: SVGProps) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox={`0 0 ${width} ${height}`}
+      fill="none"
+      onClick={onClick}
+    >
+      <path
+        d="M4.998 16H2V8H0V5.24297L2 5.24206L1.99675 3.61781C1.99675 1.36856 2.60666 0 5.25612 0H7.462V2.75756H6.08341C5.05166 2.75756 5.00203 3.14281 5.00203 3.862L4.99794 5.24206H7.4775L7.18519 7.99909L5 8L4.998 16Z"
+        fill={color}
+      />
+    </svg>
+  );
+};
+
+export default FacebookIcon;
