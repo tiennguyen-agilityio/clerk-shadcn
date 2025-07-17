baaa0533af75f4969009ee4579c10de1d40335d4
Tien Nguyen <tien.nguyen@asnet.com.vn>
2025-07-17 10:36:45 +0700

    Implement Select component

---
+++ travel-landing-page/src/components/Select/__test__/Select.test.tsx
@@ -0,0 +1,31 @@
+import { render } from "@testing-library/react";
+
+import Select from "..";
+import { MONTHS } from "@/constants";
+
+describe("Select component", () => {
+  const props = {
+    label: "Label Select",
+    placeholder: "Placeholder",
+    list: MONTHS,
+  };
+  it("should render correctly", () => {
+    const { container } = render(<Select {...props} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should render with disabled true", () => {
+    const { container } = render(<Select {...props} defaultValue={MONTHS[0]} disabled />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should render with label empty", () => {
+    const { container } = render(<Select list={MONTHS} label={undefined} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should render with size small", () => {
+    const { container } = render(<Select {...props} size="sm" />);
+    expect(container).toMatchSnapshot();
+  });
+});
---
+++ travel-landing-page/src/components/Select/index.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React, { ComponentProps, useId } from "react";
+
+import {
+  Select as ShadCNSelect,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectTriggerProps,
+  SelectValue,
+} from "@/components/ui/select";
+import { Label } from "../ui/label";
+
+type SelectProps = ComponentProps<typeof ShadCNSelect> &
+  SelectTriggerProps & {
+    list: string[];
+    label?: string;
+    placeholder?: string;
+  };
+
+const Select = ({
+  label = "",
+  placeholder = "",
+  size = "default",
+  list,
+  ...props
+}: SelectProps) => {
+  const id = useId();
+
+  return (
+    <div className="w-full">
+      {label && (
+        <Label htmlFor={id} className="pb-1.25 py-3">
+          {label}
+        </Label>
+      )}
+      <ShadCNSelect {...props}>
+        <SelectTrigger id={id} className="w-full" size={size}>
+          <SelectValue placeholder={placeholder} />
+        </SelectTrigger>
+        <SelectContent>
+          {list?.map((item, index) => (
+            <SelectItem key={index} value={item} size={size} className="">
+              {item}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </ShadCNSelect>
+    </div>
+  );
+};
+
+export default Select;
