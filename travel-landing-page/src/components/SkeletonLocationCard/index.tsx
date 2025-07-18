d050cf2ac609a03b86002b5ab87b1876b744ee3e
Tien Nguyen <tien.nguyen@asnet.com.vn>
2025-07-18 09:50:35 +0700

    Update SkeletonLocationCard component

--- travel-landing-page/src/components/SkeletonLocationCard/index.tsx
+++ travel-landing-page/src/components/SkeletonLocationCard/index.tsx
@@ -10,19 +10,13 @@ interface SkeletonLocationCard {
 const SkeletonLocationCard = ({ imageHeight = 340 }: SkeletonLocationCard) => {
   return (
     <Card className="w-full h-full p-0">
-      <CardHeader className="relative p-0" style={{ height: imageHeight }}>
+      <CardHeader className="p-0" style={{ height: imageHeight }}>
         <Skeleton className="w-full" style={{ height: imageHeight }} />
       </CardHeader>
       <CardContent className="px-7.5 pt-4 pb-6">
-        <p className="font-bold text-lg">
-          <Skeleton className="w-full h-7" />
-        </p>
-        <div className="flex items-center gap-1.25 mt-3 text-ring">
-          <Skeleton className="w-full h-6" />
-        </div>
-        <div className="flex items-center gap-1.25 mt-4 text-chart-2">
-          <Skeleton className="w-full h-6" />
-        </div>
+        <Skeleton className="w-full h-7" />
+        <Skeleton className="w-full h-6 mt-3" />
+        <Skeleton className="w-full h-6 mt-4" />
       </CardContent>
     </Card>
   );
