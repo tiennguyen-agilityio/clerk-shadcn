f5f15dadf0c705a2d35bcec85a17f7e9e126b82b
Tien Nguyen <tien.nguyen@asnet.com.vn>
2025-07-18 15:18:02 +0700

    Implement LocationTile component

---
+++ travel-landing-page/src/components/LocationTile/__test__/LocationTile.test.tsx
@@ -0,0 +1,22 @@
+import { render } from "@testing-library/react";
+
+import LocationTile from "..";
+import { LOCATION_TILES } from "@/mocks";
+
+describe("LocationTile component", () => {
+  const props = {
+    ...LOCATION_TILES[0],
+  };
+
+  it("should render correctly", () => {
+    const { container } = render(<LocationTile {...props} />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should render correctly with link", () => {
+    const { container } = render(<LocationTile {...props} link="/link" count={undefined} />);
+
+    expect(container).toMatchSnapshot();
+  });
+});
--- travel-landing-page/src/mocks/location.ts
+++ travel-landing-page/src/mocks/location.ts
@@ -12,3 +12,36 @@ export const LOCATIONS = [
     reviews: 89,
   },
 ];
+
+export const LOCATION_TILES = [
+  {
+    title: "Nairobi",
+    count: 23,
+    slug: "nairobi",
+    imageUrl: "https://i.ibb.co/hxbHQJf8/Rectangle.png",
+  },
+  {
+    title: "Kisumu",
+    count: 23,
+    slug: "kisumu",
+    imageUrl: "https://i.ibb.co/B7QhHS9/Rectangle-Copy-4.png",
+  },
+  {
+    title: "Bungee Jumping",
+    count: 23,
+    slug: "bungee-jumping",
+    imageUrl: "https://i.ibb.co/M56kRhGM/Rectangle-Copy-3.png",
+  },
+  {
+    title: "Coffee Tasting",
+    count: 23,
+    slug: "coffee-tasting",
+    imageUrl: "https://i.ibb.co/817d0wL/Rectangle-Copy-5.png",
+  },
+  {
+    title: "Culture Walk",
+    count: 23,
+    slug: "culture-walk",
+    imageUrl: "https://i.ibb.co/Z6JhM42D/Rectangle-Copy-6.png",
+  },
+];
