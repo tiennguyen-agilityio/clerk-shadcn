6e5d8df935efcb68ce3606d6fe975f8889c82364
Tien Nguyen <tien.nguyen@asnet.com.vn>
2025-07-24 08:34:24 +0700

    Update storybook for components

--- travel-landing-page/src/components/LocationTile/LocationTile.stories.tsx
+++ travel-landing-page/src/components/LocationTile/LocationTile.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/nextjs";
 
 import LocationTile from ".";
 import { LOCATION_TILES } from "@/mocks";
--- travel-landing-page/src/components/StatisticsCard/StatisticsCard.stories.tsx
+++ travel-landing-page/src/components/StatisticsCard/StatisticsCard.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/nextjs";
 
 import { STATISTICS_BY_SERVICES } from "@/constants";
 
