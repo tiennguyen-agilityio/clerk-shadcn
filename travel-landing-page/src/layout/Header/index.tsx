03ce7d61e4c994c7bab2a421de951b5cd8a4131a
Tien Nguyen <tien.nguyen@asnet.com.vn>
2025-07-28 15:43:47 +0700

    Update unit test for NavigationMenu component

--- travel-landing-page/src/layout/Header/index.tsx
+++ travel-landing-page/src/layout/Header/index.tsx
@@ -7,7 +7,6 @@ import clsx from "clsx";
 
 import { NAV_BAR } from "@/constants/nav";
 
-import NavBar from "@/components/NavBar";
 import Logo from "@/components/Logo";
 import { Button } from "@/components/ui";
 import UserDropdown from "@/components/UserDropdown";
@@ -15,6 +14,7 @@ import BookmarkIcon from "@/components/Icons/BookmarkIcon";
 import SearchIcon from "@/components/Icons/SearchIcon";
 
 import { ModalToggleTheme } from "@/themes";
+import NavigationMenu from "@/components/NavigationMenu";
 
 interface HeaderProps {
   hasBorderBottom?: boolean;
@@ -34,15 +34,15 @@ const Header = ({ hasBorderBottom = false }: HeaderProps) => {
     >
       <div className="container flex items-center content-center mx-auto h-25 text-current">
         <Logo className="w-auto hover:text-white-500" />
-        <div className="flex ml-auto mr-0 items-center">
-          <NavBar list={NAV_BAR} href={pathname} />
+        <div className="flex ml-auto mr-0 items-center gap-2 md:gap-3 lg:gap-5">
+          <NavigationMenu list={NAV_BAR} path={pathname} />
           <ModalToggleTheme />
-          <Button className="ml-20 mr-4 rounded-[20px] text-white" size="sm">
+          <Button className="lg:ml-20 rounded-[20px] text-white" size="sm">
             <BookmarkIcon />
             Reservations
           </Button>
           <SearchIcon />
-          <div className="flex justify-end w-20 min-w-20 ml-5">
+          <div className="flex justify-end w-20 min-w-20 ml-1 md:ml-2 lg:ml-5">
             <ClerkLoaded>
               <SignedOut>
                 <div className="flex items-center font-acme text-[13px]">
