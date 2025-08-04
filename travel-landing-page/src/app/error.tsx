b95c57e96957b574fbb8d7d00f0d0d5c5f7f4642
Tien Nguyen <tien.nguyen@asnet.com.vn>
2025-08-04 14:02:12 +0700

    Update show toast message when deactivate user

--- travel-landing-page/src/app/error.tsx
+++ travel-landing-page/src/app/error.tsx
@@ -6,18 +6,16 @@ import FaceFrownIcon from "@/components/Icons/FaceFrownIcon";
 
 const GlobalError = ({ reset }: { error: Error; reset: () => void }) => {
   return (
-    <main className="flex flex-col items-center justify-center h-fit pt-45 pb-25 gap-6">
-      <div className="container mx-auto">
-        <FaceFrownIcon className="size-50" />
-        <Heading as="h4" className="text-2xl font-bold text-center text-error mb-2">
-          Oops! Something went wrong.
-          <br />
-          Please try again!
-        </Heading>
-        <Button variant="destructive" onClick={() => reset()} className="px-10 py-">
-          Try again
-        </Button>
-      </div>
+    <main className="flex flex-col mx-auto items-center justify-center h-fit pt-45 pb-25 gap-6">
+      <FaceFrownIcon className="size-50" />
+      <Heading as="h4" className="text-2xl font-bold text-center text-error mb-2">
+        Oops! Something went wrong.
+        <br />
+        Please try again!
+      </Heading>
+      <Button variant="destructive" onClick={() => reset()} className="px-10 py-">
+        Try again
+      </Button>
     </main>
   );
 };
