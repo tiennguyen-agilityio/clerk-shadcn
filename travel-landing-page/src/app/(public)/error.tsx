3e702e1ae91ee7eb3c2a7bca9ce42ff69e94ecef
Tien Nguyen <tien.nguyen@asnet.com.vn>
2025-08-11 17:59:45 +0700

    Add public group routes

--- travel-landing-page/src/app/(public)/error.tsx
+++ travel-landing-page/src/app/(public)/error.tsx
@@ -5,11 +5,13 @@ import Heading from "@/components/Heading";
 
 const Error = ({ reset }: { error: Error; reset: () => void }) => {
   return (
-    <div className="flex flex-col items-center justify-center h-fit pb-15 pt-35 gap-6">
+    <div className="flex flex-col items-center justify-center h-fit pb-15 pt-35 gap-6 z-70 bg-foreground dark:bg-background">
       <Heading as="h4" className="text-2xl font-bold text-error mb-2">
         Oops! Something went wrong.
       </Heading>
-      <p className="mb-4">We couldn&rsquo;t load the camping locations page. Please try again.</p>
+      <p className="mb-4 text-accent dark:text-current">
+        We couldn&rsquo;t load the page. Please try again.
+      </p>
       <Button variant="destructive" onClick={() => reset()} className="px-10 py-">
         Try again
       </Button>
